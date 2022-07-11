043e5e87309f2192dcab66a720c244547625b0c1
Divak2004 <fu_david@outlook.com>
2022-07-10 23:14:32 -0400

    Update tkSignOut.py
    Edited ListBox so that it only displays visitors who are currently signed in.

--- v2/tkSignOut.py
+++ v2/tkSignOut.py
@@ -1,7 +1,6 @@
 # To-do list:
 # 1. Figuring out how to code the back button to go to a different page
-# 2. Only display visitors that are signed in
-# 3. Formatting
+# 2. Formatting
 
 from tkinter import *
 from backend import *
@@ -10,40 +9,43 @@ root = Tk()
 root.title('Springfield Senior Home')
 root.geometry("500x700")
 
-# Outputs list of visitors of a given apartment #
+# Takes in apt # and displays all visitors in that apt that are currently signed in
 def enter():
     message.config(text="")
     number = int(aptNumber.get())
-    # I will deal with invalid apartment numbers later
     match apts.get_apt(number):
         case Success():
             visitorList.delete(0, END)
             apt = apts.get_apt(number).unwrap()
             if not apt.visitors:
                 message.config(text="There are no registered visitors for this apartment.")
-            # Try to code to only insert visitors signed in already
-            for visitor in apt.visitors:
-                visitorList.insert(END, visitor)
+            else:
+                hasVisitors = False
+                for visitor in apt.visitors:
+                    if (number, visitor) in allVisitors.visitors:
+                        visitorList.insert(END, visitor)
+                        hasVisitors = True
+                if(hasVisitors == False):
+                    message.config(text="There are no visitors currently signed in for this apartment.")
         case Failure():
             visitorList.delete(0, END)
             message.config(text="Please enter a valid apartment number.\nAsk help from the guard if necessary.")
 
-# Signs out the selected visitor if the visitor is signed in
+# Signs out the selected visitor
 def signOut():
     number = int(aptNumber.get())
     name = visitorList.get(ANCHOR)
-    allVisitors.sign_in(number, name)
-    match allVisitors.sign_out(number, name):
-        case Success():
-            allVisitors.sign_out(number, name)
-            message.config(text=name + " has successfully signed out.")
-        case Failure():
-            message.config(text="This visitor is not signed in in the first place.")
+    allVisitors.sign_out(number, name)
+    if not visitorList.curselection():
+        message.config(text="Please select a visitor to sign out.")
+    else:
+        message.config(text=name + " has successfully signed out.")
+        visitorList.delete(ANCHOR)
 
 # Help message that appears when help button is clicked
 def help():
     message.config(text="To sign out, enter the apartment number of the visitor.\n \
-Click 'Enter'.\nYou will see a list of all the registered visitors in the box above.\n \
+Click 'Enter'.\nYou will see a list of all the registered visitors who are currently signed in in the box above.\n \
 Click on the visitor that you want to sign out.\nClick 'Sign out'.\n \
 If you don't see your name, please register yourself as a visitor in the sign in page.")
 
