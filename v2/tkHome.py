a07c758b20651455f1acd34cecb71725c10a7682
Broyojo <dha@xoba.com>
2022-08-07 17:42:25 -0400

    improved home screen that can open different workflows

--- v2/tkHome.py
+++ v2/tkHome.py
@@ -1,11 +1,23 @@
 import tkinter as tk
+import sys
+import os
+from tkinter import *
 from PIL import ImageTk
 
 bg_color = "#cd661d"
 
 
-def load_frame2():
-    print("Take me to the next page")  # need to link to sign-in page
+def openSignIn():
+    os.system('python3 tkSignIn.py')  # calls the tkSignIn.py python script
+
+
+def openSignOut():
+    os.system('python3 tkSignOut.py')  # calls the tkSignOut.py python script
+
+
+def openUpdateProfile():
+    # calls the tkUpdateProfile.py python script
+    os.system('python3 tkUpdateProfile.py')
 
 
 # initialize app
@@ -14,32 +26,53 @@ root.title("Springfield Senior Center Homepage")
 root.eval("tk::PlaceWindow . center")
 
 # creating a frame widget
-frame1 = tk.Frame(root, width=1310, height=980, bg=bg_color)
-frame1.grid(row=0, column=0)
-frame1.pack_propagate(False)
+homeFrame = tk.Frame(root, width=1310, height=980, bg=bg_color)
+homeFrame.grid(row=0, column=0)
+homeFrame.pack_propagate(False)
 
-# frame1 widgets
-logo_img = ImageTk.PhotoImage(file="./images/frontentrance.jpeg")
-logo_widget = tk.Label(frame1, image=logo_img, bg=bg_color)
+# homeFrame widgets
+logo_img = ImageTk.PhotoImage(
+    file="./images/frontentrance.jpeg")  # homepage image
+logo_widget = tk.Label(homeFrame, image=logo_img, bg=bg_color)
 logo_widget.image = logo_img
 logo_widget.pack()  # pack method
 
 # label widget
-tk.Label(frame1, text="Welcome to the Springfield Senior Center Page!",
+tk.Label(homeFrame, text="Welcome to the Springfield Senior Center Page!",
          bg=bg_color,
          fg="white",
          font=("TkMenuFont", 16)
          ).pack(pady=10)
 
 # button widget
-tk.Button(frame1, text="SIGN-IN HERE",
+tk.Button(homeFrame, text="SIGN-IN",
           font=("TkHeadingFont", 20),
           bg="#28393a",
           fg="white",
           cursor="hand2",
           activebackground="#e3cf57",
           activeforeground="black",
-          command=lambda: load_frame2()
+          command=openSignIn
+          ).pack(pady=20)
+
+tk.Button(homeFrame, text="SIGN-OUT",
+          font=("TkHeadingFont", 20),
+          bg="#28393a",
+          fg="white",
+          cursor="hand2",
+          activebackground="#e3cf57",
+          activeforeground="black",
+          command=openSignOut
+          ).pack(pady=20)
+
+tk.Button(homeFrame, text="UPDATE PROFILE",
+          font=("TkHeadingFont", 20),
+          bg="#28393a",
+          fg="white",
+          cursor="hand2",
+          activebackground="#e3cf57",
+          activeforeground="black",
+          command=openUpdateProfile
           ).pack(pady=20)
 
 # run app // displays window until EXIT
