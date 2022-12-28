d67998a5c134c43f3e1f6d22bc7194ca35a0c75f
Broyojo <dha@xoba.com>
2022-12-28 11:47:41 -0500

    made v1 branch

--- v2/test.py
+++
@@ -1,9 +0,0 @@
-from backend import *
-
-vm = VisitorManager(ApartmentDatabase("./data/apts.json"), "./visitor-logs")
-vm.sign_in("david", 102).unwrap()
-vm.sign_out("david", 102).unwrap()
-vm.sign_in("bob", 102).unwrap()
-vm.sign_in("clara", 102).unwrap()
-vm.sign_out("bob", 102).unwrap()
-vm.sign_out("clara", 102).unwrap()
